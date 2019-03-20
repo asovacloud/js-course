82c8a529dc1697caf2c74cf0085d87caf8587668
Andrew Sova <andrew.sova@mabbly.com>
2019-03-20 21:40:49 +0200

    feat(js): Fixed Edit_todo_item

--- todo/js/app.js
+++ todo/js/app.js
@@ -18,24 +18,16 @@ let storage = {
 };
 
 /**
- * generate_id - создает произвольную строку 
+ * generate_id - создает произвольную строку
  * @returns {string} - новый id
  */
 const generate_id = () => {
-    const words = '0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
-    let id = "";
-
-    for (let i = 0; i < 10; i++) {
-        let index = Math.floor(Math.random() * words.length);
-        id += words[index];
-    }
-
-    return id;
+    return storage.current_todos.length;
 }
 
 /**
  * add_new_todo - функция для добавления новой задачи
- * @param {String} title - заголовок задачи 
+ * @param {String} title - заголовок задачи
  * @param {String} text - текст задачи
  * @returns {void}
  */
@@ -52,22 +44,38 @@ const add_new_todo = (title, text) => {
 
 /**
  * delete_todo_item - удаление одной задачи
- * @param {sting} id 
+ * @param {sting} id
  */
-const delete_todo_item = id => {
-    if (!id) return console.log("Передайте id удаляемой задачи.");
 
-    storage.current_todos = storage.current_todos.filter(todo => todo.id !== id);
+const delete_todo_item = (id) => {
+    if (!id && id !== 0) return console.log("Передайте id удаляемой задачи.");
+
+    storage.current_todos.splice(id, 1);
+
+    let fix_id = arr => {
+        arr.forEach( (item, i) => storage.current_todos[i].id = i );
+        return null;
+    }
+
+    console.log( fix_id( storage.current_todos ) );
 
     return storage.current_todos;
+
 }
 
 /**
- * 
- * @param {*} id 
- * @param {*} title 
- * @param {*} text 
+ *
+ * @param {*} id
+ * @param {*} title
+ * @param {*} text
  */
 const edit_todo_item = (id, title, text) => {
-    
+    if (!id && id !== 0) return console.log("Введите ID.");
+    if (!title) return console.log("Введите заголовок задачи.");
+    if (!text) return console.log("Введите текст задачи.");
+
+    storage.current_todos[id].title = title;
+    storage.current_todos[id].text = text;
+
+    return storage.current_todos;
 }
