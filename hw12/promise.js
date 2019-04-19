4b686bbb4afaa5e527eeacfeed6467b2696cc7e6
Andrew Sova <andrew.sova@mabbly.com>
2019-04-20 01:20:14 +0300

    feat(js): Home work 13

--- hw12/promise.js
+++ hw12/promise.js
@@ -1,11 +1,10 @@
 // 1. Create function with timeout
 function promiseCreator( time, message) {
-    const promise = new Promise(function (resolve, reject){
+    return new Promise(function (resolve, reject){
         setTimeout( function () {
             resolve(message)
         }, time );
     });
-    return promise;
 };
 
 
@@ -15,3 +14,29 @@ prom
     .then( (value) => console.log(value))
     .catch((err) => console.log(err));
 
+// 2. Create class with promise
+
+class Prom {
+
+    constructor() {
+        this.mesage = ''
+
+        this.promise = new Promise((resolve, reject) => {
+
+            setTimeout(() => resolve( this.mesage ), 1000);
+
+        });
+
+    }
+
+    resolve( value ) {
+        return this.mesage = value;
+    }
+
+}
+
+const inst = new Prom();
+
+inst.promise.then(data => console.log( `Response is ${data}` ));
+
+inst.resolve('test');
