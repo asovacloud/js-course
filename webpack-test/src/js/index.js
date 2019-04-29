108b491682ce5fcdb057545ded1b1e8cd6be109d
Andrew Sova <andrew.sova@mabbly.com>
2019-04-29 21:56:25 +0300

    feat(js scss): Add WebPack test

--- webpack-test/src/js/index.js
+++ webpack-test/src/js/index.js
@@ -1,13 +1,68 @@
-import _ from 'lodash';
 import '../scss/style.scss';
+
+import VideoFile from '../video/movie1.mp4';
+
+import videoPlay from '../js/main.js';
 
 function component() {
     const element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack', 'Andrew', 'created', 'this! And He is the best !!! Very good !!!!!'], ' ');
-    element.classList.add('hello');
+    const source = document.createElement('source');
+
+    source.src = VideoFile;
+
+    source.type = 'video/mp4';
+
+    console.log( source );
+
+    element.classList.add('video-box');
+
+    element.innerHTML = `
+        <div class="video-box__holder">
+                <video id="myVideo">
+                    Your browser does not support the video tag.
+                </video>
+                <div class="holder-play"><i class="fa fa-play"></i></div>
+                <div class="progress-bar">
+                    <div class="progress-bar__holder">
+                        <span class="bar"></span>
+                    </div>
+                </div>
+            </div>
+            <div class="toolbar">
+                <ul class="btn-list">
+                    <li>
+                        <button class="btn btn-play dynamic-ico">
+                            <i class="fa fa-play second"></i>
+                            <i class="fa fa-pause first"></i>
+                        </button>
+                    </li>
+                    <li>
+                        <button class="btn btn-stop">
+                            <i class="fa fa-stop"></i>
+                        </button>
+                    </li>
+                </ul>
+                <div class="volume-box">
+                    <button class="btn btn-volume dynamic-ico">
+                        <i class="fa fa-volume-up second"></i>
+                        <i class="fa fa-volume-off first"></i>
+                    </button>
+                    <div class="volume-box__holder">
+                        <div class="volume-box__holder__center">
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+
+    element.querySelector('#myVideo').appendChild(source);
 
     return element;
 }
 
 document.body.appendChild(component());
+
+document.body.appendChild( videoPlay() );
--- webpack-test/webpack.config.js
+++ webpack-test/webpack.config.js
@@ -8,17 +8,24 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [
-              'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                  'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(mp4)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
     }
 };
