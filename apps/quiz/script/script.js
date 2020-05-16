9e73cf08f855df12fec0d4dc5fbe818b2b6a92d0
Andrew <asovacloud@gmail.com>
2020-05-16 08:43:23 +0300

    feat(Quiz): Add burger

--- apps/quiz/script/script.js
+++ apps/quiz/script/script.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   const btnOpenModal = document.getElementById('btnOpenModal');
   const modalBlock = document.getElementById('modalBlock');
-  const closeModal = document.getElementById('closeModal');
+  const btnCloseModal = document.getElementById('closeModal');
   const questionTitle = document.getElementById('question');
   const formAnswers = document.getElementById('formAnswers');
+  const burgerBtn = document.getElementById('burger');
+
+  let clientWidth = document.documentElement.clientWidth;
+  burgerBtn.style.display = 'none';
+
+  if(clientWidth < 768) {
+    burgerBtn.style.display = 'flex';
+  } else {
+    burgerBtn.style.display = 'none';
+  }
+
   const data = [
     { id: 1, title: 'Standart', image: './image/burger.png', },
     { id: 2, title: 'Black', image: './image/burgerBlack.png', },
@@ -12,6 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const playTest = () => {
     const renderQuestions = () => {
       questionTitle.textContent = 'Choose a color of the burger, please.';
+      formAnswers.textContent = '';
       data.forEach((item, idx) => {
         const div = document.createElement('div');
         div.classList.add('answers-item');
@@ -29,16 +41,45 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     };
     renderQuestions();
+  };
+
+  const closeModalClickOnMask = event => {
+    if (event.target.id === 'modalBlock') closeModal();
   }
 
-  btnOpenModal.addEventListener('click', () => {
+  const openModal = () => {
     modalBlock.classList.add('d-block');
     playTest();
-  });
 
-  closeModal.addEventListener('click', () => {
+    window.addEventListener('click', closeModalClickOnMask);
+  };
+
+  const closeModal = () => {
+    burgerBtn.classList.remove('active');
     modalBlock.classList.remove('d-block');
+    burgerBtn.classList.remove('active');
+
+    window.removeEventListener('click', closeModalClickOnMask);
+  };
+
+  window.addEventListener('resize', () => {
+    clientWidth = document.documentElement.clientWidth;
+    console.log("clientWidth", clientWidth)
+    if(clientWidth < 768) {
+      burgerBtn.style.display = 'flex';
+    } else {
+      burgerBtn.style.display = 'none';
+    }
   });
 
+  burgerBtn.addEventListener('click', () => {
+    burgerBtn.classList.add('active');
+
+    openModal();
+  });
+
+  btnOpenModal.addEventListener('click', openModal);
+
+  btnCloseModal.addEventListener('click', closeModal);
 
 })
